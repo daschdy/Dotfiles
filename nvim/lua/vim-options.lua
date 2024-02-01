353ca35fe43b42c197a2b1ad5741b5bb880c708e
daschdy <dustin.heither@gmail.com>
2024-02-01 20:56:22 +0100

    change line numbering

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -3,8 +3,10 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+vim.opt.cursorline = true
 vim.g.mapleader = " "
-vim.wo.relativenumber = true
+-- vim.wo.relativenumber = true
+vim.opt.number = true
 
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
