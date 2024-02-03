320de183e801058df484cf94ce088dbc5a5b86e9
daschdy <dustin.heither@gmail.com>
2024-02-03 20:38:48 +0100

    neotree: autohide dotfiles = false

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,12 +1,22 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false,
+        },
+      }
+    })
+	end,
 }
--- nvim/lua/plugins/vscode.lua
+++ nvim/lua/plugins/vscode.lua
@@ -3,7 +3,7 @@ return {
   name = "vscode",
   priority = 1000,
   config = function()
-    local c = require("vscode.colors").get_colors()
+    require("vscode.colors").get_colors()
     require("vscode").setup({
       transparent = true,
       italic_comments = true,
