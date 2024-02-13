63db24f94c29750a3511eb9142a792c873f9cbb4
daschdy <dustin.heither@gmail.com>
2024-02-13 13:13:20 +0100

    aligning themes

--- nvim/lua/plugins/vscode.lua
+++ nvim/lua/plugins/vscode.lua
@@ -1,15 +1,14 @@
 return {
-	"Mofiqul/vscode.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("vscode").load()
-		require("vscode").setup({
-			style = "dark",
-			transparent = true,
-			italic_comments = true,
-			disable_nvimtree_bg = true,
-		})
-		vim.cmd.colorscheme("vscode")
-	end,
+  "Mofiqul/vscode.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("vscode").setup({
+      style = "dark",
+      transparent = false,
+      italic_comments = true,
+    })
+    require("vscode").load()
+    vim.cmd.colorscheme("vscode")
+  end,
 }
