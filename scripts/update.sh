532d0eca725572a34cc70fcc9c7e722b42890dd2
daschdy <dustin.heither@gmail.com>
2024-02-21 13:07:00 +0100

    finalzing update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,27 +1,33 @@
-#!/bin/bash
+#!/bin/zsh
+
+## Sourcing .zshrc for sdkman and zap
+source ~/Developer/DotfilesMac/zsh/.zshrc
 
 ## Update Homebrew 
-echo "Updating Homebrew ..." 
+echo "Updating Homebrew ..."
+echo ""
 brew update 
 brew upgrade 
 
 echo "Cleaning Homebrew ..."
+echo ""
 brew cleanup 
 brew autoremove 
 
 ## SKDMAN 
 echo "Updating sdkman ..."
+echo ""
 sdk update
 
 ## RustUp 
 echo "Updating Rust ..."
+echo ""
 rustup update 
 
 ## Zap 
 echo "Updating Zap ..."
+echo ""
 zap update self 
 zap update plugins 
-
-## NEOVIM
 
 echo "You are up to date!!!"
