a4a1a3a96b42a00044775f99b00a72ac1fa48a51
daschdy <dustin.heither@gmail.com>
2024-03-05 14:47:55 +0100

    new colorscheme - getting darker

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,32 +1,41 @@
 return {
-  {
-    -- "Mofiqul/vscode.nvim",
-    -- lazy = false,
-    -- priority = 1000,
-    -- config = function()
-    --   require("vscode").setup({
-    --     style = "dark",
-    --     transparent = false,
-    --     italic_comments = true,
-    --   })
-    --   require("vscode").load()
-    --  vim.cmd.colorscheme("vscode")
-    -- end,
-  },
-  {
-    "oxfist/night-owl.nvim",
+	{
+		-- "Mofiqul/vscode.nvim",
+		-- lazy = false,
+		-- priority = 1000,
+		-- config = function()
+		--   require("vscode").setup({
+		--     style = "dark",
+		--     transparent = false,
+		--     italic_comments = true,
+		--   })
+		--   require("vscode").load()
+		--  vim.cmd.colorscheme("vscode")
+		-- end,
+	},
+	{
+		-- "oxfist/night-owl.nvim",
+		-- lazy = false,
+		-- priority = 1000,
+		-- config = function()
+		--   require("night-owl").setup({
+		--     bold = true,
+		--     italics = true,
+		--     underline = true,
+		--     undercurl = true,
+		--     transparent_background = false,
+		--   })
+		--
+		--   vim.cmd.colorscheme("night-owl")
+		-- end,
+	},
+	{
+    "shatur/neovim-ayu",
     lazy = false,
     priority = 1000,
     config = function()
-      require("night-owl").setup({
-        bold = true,
-        italics = true,
-        underline = true,
-        undercurl = true,
-        transparent_background = false,
-      })
-
-      vim.cmd.colorscheme("night-owl")
+      vim.g.ayu_mirage = false
+      vim.cmd.colorscheme("ayu")
     end,
-  },
+	},
 }
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "night-owl",
+				theme = "ayu",
 			},
 		})
 	end,
