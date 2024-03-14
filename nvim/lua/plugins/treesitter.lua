237d5e61b86dcdbcf581914e15824a9ec2caf40e
daschdy <dustin.heither@gmail.com>
2024-03-14 15:12:34 +0100

    markdownpreviewtoggel keys

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,30 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = {
-        "rust",
-        "toml",
-        "java",
-        "astro",
-        "tsx",
-        "typescript",
-        "html",
-        "markdown",
-        "markdown_inline",
-        "scala",
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = {
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"java",
+				"scala",
+				"rust",
+				"toml",
+				"astro",
+				"typescript",
+				"javascript",
+				"tsx",
+				"html",
+			},
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
+		})
+	end,
 }
