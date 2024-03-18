19a76695c0ca719e9fc0909b29281e9160b8ff13
daschdy <dustin.heither@gmail.com>
2024-03-18 18:32:02 +0100

    cleaning plugings

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,22 +1,19 @@
-local M = {
+return {
 	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+
+		local formatting = null_ls.builtins.formatting
+
+		null_ls.setup({
+			debug = true,
+			sources = {
+				formatting.stylua,
+				formatting.prettier,
+				formatting.markdownlint,
+				formatting.yapf,
+				null_ls.builtins.completion.spell,
+			},
+		})
+	end,
 }
-
-function M.config()
-	local null_ls = require("null-ls")
-
-	local formatting = null_ls.builtins.formatting
-
-	null_ls.setup({
-		debug = true,
-		sources = {
-			formatting.stylua,
-			formatting.prettier,
-      formatting.markdownlint,
-      formatting.yapf,
-			null_ls.builtins.completion.spell,
-		},
-	})
-end
-
-return M
