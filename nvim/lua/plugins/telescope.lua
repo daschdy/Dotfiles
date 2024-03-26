0793046a48e63ab9aa618447b80e0ab48292d9f6
daschdy <dustin.heither@gmail.com>
2024-03-26 21:20:29 +0100

    find hidden files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,44 +1,47 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    event = "VeryLazy",
-    tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup({
-        defaults = {
-          vimgrep_arguments = {
-            "rg",
-            "--color=never",
-            "--no-heading",
-            "--with-filename",
-            "--line-number",
-            "--column",
-            "--smart-case",
-            "-u",
-            "--hidden",
-          },
-        },
-      })
+	{
+		"nvim-telescope/telescope.nvim",
+		event = "VimEnter",
+		branch = "0.1.x",
+		dependencies = {
+			{
+				"nvim-lua/plenary.nvim",
+			},
+			{
+				"nvim-telescope/telescope-fzy-native.nvim",
+				build = "make",
+				cond = function()
+					return vim.fn.executable("make") == 1
+				end,
+			},
+			{
+				"nvim-telescope/telescope-ui-select.nvim",
+			},
+			{
+				"nvim-tree/nvim-web-devicons",
+			},
+		},
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown(),
+					},
+				},
+			})
 
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-    end,
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({
-              require("telescope").load_extension("ui-select"),
-            }),
-          },
-        },
-      })
-    end,
-  },
+			pcall(require("telescope").load_extension, "fzf")
+			pcall(require("telescope").load_extension, "ui-select")
+
+			local builtin = require("telescope.builtin")
+			builtin.find_files({
+				find_command = { "rg", "--files", "--iglob", "!.git", "--hidden" },
+				previewer = false,
+			})
+
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[ ] Find existing buffers" })
+		end,
+	},
 }
