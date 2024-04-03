ff7e7475d1107898cefd78bf3142c8ef3ec2b2f8
daschdy <dustin.heither@gmail.com>
2024-04-03 20:48:55 +0200

    outsourcing nvim

--- nvim/lua/daschdy/init.lua
+++
@@ -1,4 +0,0 @@
-require("daschdy.autocmd")
-require("daschdy.options")
-require("daschdy.lazy")
-require("daschdy.keymapping")
--- nvim/lua/plugins/zettelkasten.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "renerocksai/telekasten.nvim",
-  dependencies = { "nvim-telescope/telescope.nvim" },
-  config = function()
-    require("telekasten").setup({
-      home = vim.fn.expand("/Users/dustin/Zettelkasten/"),
-    })
-  end,
-}
