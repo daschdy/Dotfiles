d485de2bef1b2d34d62db5fb9558e11c59fc1c3c
daschdy <dustin.heither@gmail.com>
2024-05-15 22:20:34 +0200

    rewriting wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,32 +3,53 @@ local config = wezterm.config_builder()
 local mux = wezterm.mux
 local act = wezterm.action
 
-local usedFont = "Anonymous Pro"
-local usedFontSize = 18
-
 -- Window is maximized on startup
 wezterm.on("gui-startup", function()
 	local tab, pane, window = mux.spawn_window({})
 	window:gui_window():maximize()
 end)
 
--- config.color_scheme = "rose-pine"
-config.term = "xterm-256color"
-config.window_background_opacity = 1
-config.font = wezterm.font(usedFont)
-config.font_size = usedFontSize
-config.line_height = 1.3
-config.enable_tab_bar = true
+-- general settings
 config.enable_scroll_bar = false
-config.tab_bar_at_bottom = false
 config.scrollback_lines = 10000
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
 config.audible_bell = "Disabled"
 
+-- colorscheme
+config.color_scheme = "Moonfly (Gogh)"
+config.term = "xterm-256color"
+config.window_background_opacity = 1
+
+-- Font settings
+config.font_size = 16
+config.line_height = 1.3
+config.font = wezterm.font({ family = "Fira Code", weight = "Regular" })
+config.bold_brightens_ansi_colors = true
+config.font_rules = {
+	{
+		intensity = "Bold",
+		italic = true,
+		font = wezterm.font({ family = "Maple Mono", weight = "Bold", style = "Italic" }),
+	},
+	{
+		italic = true,
+		intensity = "Half",
+		font = wezterm.font({ family = "Maple Mono", weight = "DemiBold", style = "Italic" }),
+	},
+	{
+		italic = true,
+		intensity = "Normal",
+		font = wezterm.font({ family = "Maple Mono", style = "Italic" }),
+	},
+}
+
+-- tab bar settings
+config.enable_tab_bar = true
+config.tab_bar_at_bottom = false
 config.window_frame = {
-	font = wezterm.font({ family = usedFont, weight = "Bold" }),
-	font_size = 14.5,
+	font = wezterm.font({ family = "Fira Code", weight = "Bold" }),
+	font_size = 12,
 	active_titlebar_bg = "#333333",
 	inactive_titlebar_bg = "#333333",
 }
@@ -39,6 +60,7 @@ config.colors = {
 	},
 }
 
+-- make tmux functions
 config.leader = { key = "s", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
 	-- Create a new tab in the same domain as the current pane.
